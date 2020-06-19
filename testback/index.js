ffb3f19c05ef2119a8e00840db1f02224b43a159
Ashwani <av11000100@gmail.com>
2020-06-20 01:06:58 +0530

    install express and nodemon

---
+++ testback/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+const port = 8000;
+
+app.get("/", (req, res) => {
+    return res.send("Hello there!!");
+});
+app.get("/signout", (req, res) => {
+    return res.send("You are signed out");
+});
+app.get("/new", (req, res) => {
+    return res.send("just something new");
+});
+
+app.listen(port, () => {
+    console.log("Server is up and running at port 8000...");
+});
+
+// const port = 3000
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
