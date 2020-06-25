3e3acc7f7776cb9d0d2170474f8b6f735f178b23
Ashwani <av11000100@gmail.com>
2020-06-26 03:52:22 +0530

    Complete user schema

--- backendproj/models/user.js
+++ backendproj/models/user.js
@@ -1,15 +1,76 @@
 var mongoose = require('mongoose');
 //var Schema = mongoose.Schema;
+const crypto = require('crypto');
+const uuidv1= require('uuid/v1');
 
 var userSchema = new mongoose.Schema({
-  title:  String, // String is shorthand for {type: String}
-  author: String,
-  body:   String,
-  comments: [{ body: String, date: Date }],
-  date: { type: Date, default: Date.now },
-  hidden: Boolean,
-  meta: {
-    votes: Number,
-    favs:  Number
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    maxlength: 32,
+    trim: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true
+  },
+  userinfo: {
+    type: String,
+    trim: true
+  },
+  encrypt_password: {
+    type: String,
+    required: true
+  },
+  salt: {
+    type: String
+  },
+  role :{
+    type: Number,
+    default: 0
+  },
+  purchases: {
+    type: Array,
+    dafault: []
   }
+
 });
+userSchema.virtual("password")
+  .set(function(password){
+    this._password = password
+    this.salt = uuidv1();
+    this.encrypt_password = this.getSecurePass(password);
+  })
+  .get(function(){
+    return this._password;
+  })
+
+
+userSchema.method = {
+  authenticate: function(plainpassword){
+    return this.getSecurePass(plainpassword) === this.encrypt_password;
+  },
+
+  getSecurePass: function(plainpassword){
+    if(!password) return "";
+    try {
+        return crypto.createHmac('sha256', this.salt)
+        .update(plainpassword)
+        .digest('hex');
+    }
+    catch(err) {
+      return "";
+    }
+  }
+}
+
+
+
+module.exports = mongoose.model("User", userSchema)
